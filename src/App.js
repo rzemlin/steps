df4b89e11c469e557b27a13ad2afeecc1b6304f2
Ryan Zemlin <rzemlin@gmail.com>
2023-11-21 20:42:33 -0800

    done

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 
 const messages = [
   "Learn React ⚛️",
@@ -5,10 +6,18 @@ const messages = [
   "Invest your new income 🤑",
 ];
 export default function App() {
-  const step = 1;
+  const [step, setStep] = useState(1)
+
+  function handlePrevious() {
+    if (step > 1) setStep(step - 1);
+  }
+  function handleNext() {
+    if (step < 3) setStep(step + 1);
+  }
+
   return (
-    <div className="steps">
-      <div className="numbers">
+        <div className="steps">
+    <div className="numbers">
         <div className={`${step >= 1 ? "active" : ""}`}>1</div>
         <div className={`${step >= 2 ? "active" : ""}`}>2</div>
         <div className={`${step >= 3 ? "active" : ""}`}>3</div>
@@ -16,11 +25,11 @@ export default function App() {
       <p className="message">Step {step}: {messages[step-1]}</p>
       <div className="buttons">
         <button style={{ backgroundColor: "#7950f2", color: "#fff" }}
-        onClick={() => alert("previous")}>
+        onClick={handlePrevious}>
           previous
         </button>
         <button style={{ backgroundColor: "#7950f2", color: "#fff" }} 
-        onClick={() => alert("next")}>
+        onClick={handleNext}>
           next
         </button>
       </div>
